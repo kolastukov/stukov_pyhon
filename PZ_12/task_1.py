9bf142f731252bef2b55ecb2387ab70ba4c5a426
kolastukov <kolasystinov@gmail.com>
2025-06-09 12:30:24 +0300

    Переделал 12 практическую

--- PZ_12/task_1.py
+++ PZ_12/task_1.py
@@ -1,16 +1,39 @@
 #Дана последовательность целых чисел. Поменять местами ее первую и последнюю трети.
 #Составить генератор (yield), который выводит из строки только цифры.
 
-def swap_elements(list_to_swap, index1, index2):
+#Задание 1
+spisok = list((1, 2, 3, 4, 5, 6, 7, 8, 9))
+print(spisok)
 
-    if index1 < 0 or index1 >= len(list_to_swap) or index2 < 0 or index2 >= len(list_to_swap):
-        return list_to_swap
+def rasdelenie(spisok):
+    n = len(spisok)
+    treti = n // 3
+    tret_1 = spisok[:treti]
+    tret_2 = spisok[treti:treti * 2]
+    tret_3 = spisok[treti * 2:]
+    return tret_3 + tret_2 + tret_1
 
-    new_list = list_to_swap[:]
-    new_list[index1], new_list[index2] = new_list[index2], new_list[index1]
-    return new_list
+delen = rasdelenie(spisok)
+print(delen)
 
-my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-swapped_list = swap_elements(my_list, 0, 8)
-print(my_list)
-print(swapped_list)
+#Задание 2
+text = "1а, 2б, 3в, 4г, 5д, 6е, 7ё, 8ж, 9з"
+def generator(text):
+    for symbol in text:
+        if symbol.isdigit():
+            yield symbol
+start_generator = generator(text)
+for digit in start_generator:
+    print(digit)
+
+
+
+
+
+
+
+
+
+
+
+
