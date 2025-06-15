d4834fd0f68e34aca6aee53e44926252b518d4a9
kolastukov <kolasystinov@gmail.com>
2025-06-15 12:12:26 +0300

    Полностью изменил 13 практическую

--- PZ_13/task_1.py
+++ PZ_13/task_1.py
@@ -1,16 +1,18 @@
 #В двумерном списке найти сумму элементов второй половины матрицы.
 
-def sum_elements(MyList, n):
-    suma = 0
-    polovina = n // 2
-    polovina_2 = MyList[polovina:polovina * 2]
-    print("Вторая половина матрици ", polovina_2)
+matricha = [
+    [1, 2, 3, -4],
+    [8, 7, -6, 5],
+    [22,-13, 7, 1],
+    [12, 14, -5, 21]
+]
 
-    for row in polovina_2:
-        suma += sum(row)
-    print("Сумма чисел во второй половине матрицы ", suma)
+half = len(matricha) // 2
+two_polovina = matricha[half:]
 
-MyList = [[10, 20], [30, 40]]
-print("Матрица ", MyList)
-n = len(MyList)
-sum_elements(MyList, n)
+suma_elementov = sum(map(lambda rad_matrichi: sum(rad_matrichi), two_polovina))
+
+print("Начальная матрица:")
+for rad_matrichi in matricha:
+    print(rad_matrichi)
+print(f"Сумма элементов второй половины матрицы (нижней половины): {suma_elementov}")
--- PZ_13/task_2.py
+++ PZ_13/task_2.py
@@ -1,10 +1,17 @@
 #В двумерном списке элементы второго столбца возвести в квадрат.
 
-MyList = [[10, 20], [30, 40]]
-print("Матрица ", MyList)
+matricha = [
+    [1, 2, 3, -4],
+    [8, 7, -6, 5],
+    [22,-13, 7, 1],
+    [12, 14, -5, 21]
+]
 
-column = 2
-for i in MyList:
-    print(i[column-1])
-    i = i[column-1] ** 2
-    print("Степень", i)
+matricha_v_kvadrate = [
+    [rad_matrichi[i] ** 2 if i == 1 else rad_matrichi[i] for i in range(len(rad_matrichi))]
+    for rad_matrichi in matricha
+]
+
+print("\nМатрица после возведения второго столбца в квадрат:")
+for rad_matrichi in matricha_v_kvadrate:
+    print(rad_matrichi)
